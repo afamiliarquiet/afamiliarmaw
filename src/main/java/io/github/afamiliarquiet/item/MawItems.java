d5cbccd89a664d2608765774222f88417bb009c2
afamiliarquiet <afamiliarquiet@gmail.com>
2024-06-19 23:04:49 -0700

    curious vials are obtainable in survival now! woo. have fun doing ominous trials for a party trick :innocent:
    (really it's less for the party trick and more for the tf that i want it to be rare and special.)

--- src/main/java/io/github/afamiliarquiet/item/MawItems.java
+++ src/main/java/io/github/afamiliarquiet/item/MawItems.java
@@ -1,9 +1,15 @@
 package io.github.afamiliarquiet.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -31,10 +37,22 @@ public class MawItems {
     public static void register() {
         registerItem(CURIOUS_VIAL_ID, CURIOUS_VIAL);
 
-        // todo - add source for curious vial - vaults? wandering traders?
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(
                 content -> content.addAfter(Items.OMINOUS_BOTTLE, CURIOUS_VIAL)
         );
+
+        // technically obtainable. would you really want to do this? probably not!
+        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+            if (source.isBuiltin() && key.equals(LootTables.TRIAL_CHAMBERS_REWARD_OMINOUS_COMMON_CHEST)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .with(ItemEntry.builder(CURIOUS_VIAL)
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 6))));
+
+                tableBuilder.pool(poolBuilder);
+            }
+        });
+
+
 
         registerItem(CHOMPED_WOODEN_SWORD_ID, CHOMPED_WOODEN_SWORD);
         registerItem(CHOMPED_STONE_SWORD_ID, CHOMPED_STONE_SWORD);
